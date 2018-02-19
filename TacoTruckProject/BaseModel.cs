fd9182bbdcc02ea9f0abdcf3a628dc03f0d5191f
Zach Schindler <zgs93@yahoo.com>
2018-02-19 16:23:40 -0500

    Added Models to the project

---
+++ TacoTruckProject/BaseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacoTruckProject
+{
+    class BaseModel
+    {
+        public bool Flour_Tortilla { get; set; }
+
+        public bool Wheat_Tortilla { get; set; }
+
+        public bool Chips { get; set; }
+
+        public bool Salad { get; set; }
+    }
+}
---
+++ TacoTruckProject/SidesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacoTruckProject
+{
+    class SidesModel
+    {
+        public bool RiceBeans { get; set; }
+
+        public bool ChipSalsa { get; set; }
+
+        public bool ChipQueso { get; set; }
+
+        public bool ChipsGuac { get; set; }
+    }
+}
